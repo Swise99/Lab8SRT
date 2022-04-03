ab6e7061a600506107e6421003f24ca33c4010ee
Swise99 <samuel@inprof.com>
2022-04-02 21:19:27 -0400

    application works with simple html views

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const getJSONString = obj => {
     return JSON.stringify(obj,null,2);
 }
+const sendErrorResponse = res => {
+    res.writeHead(httpStatus.NOT_FOUND, {
+        "Content-Type": "text/html"
+    });
+    res.write("<h1>Page Not found</h1>")
+    res.end();
+}
 const port = 3000;
-
-http= require("http");
+fs=require("fs");
+http=require("http");
 httpStatus=require("http-status-codes");
-app=http.createServer() 
-app.on("request",(req,res) => {
-    var body =[];
+http.createServer((req,res) => {
+    var body =[]; // Handle and record request data and metadata
     req.on("data", (bodyData) => {
         body.push(bodyData);
     });
@@ -18,14 +24,30 @@ app.on("request",(req,res) => {
     console.log(`Method: ${getJSONString(req.method)}`);
     console.log(`URL: ${getJSONString(req.url)}`);
     console.log(`Headers: ${getJSONString(req.headers)}`);
-    res.writeHead(httpStatus.OK, {
-        "Content-Type": "text/html"
-    });
-    if (routeResponseMap[req.url]) {
-        setTimeout(() => res.end(routeResponseMap[req.url]), 10000);
+    let url = req.url;
+    if (url.indexOf(".html") !== -1) {
+        res.writeHead(httpStatus.OK, {
+            "Content-Type": "text/html"
+        });
+        customReadFile(`./views${url}`, res);      
     } else {
-        res.end("<h1> Welcome! </h1>")
+        sendErrorResponse(res);
     }
-});
-app.listen(port);
+}).listen(port);
 console.log(`Our server is listening on port number: ${port}`);
+
+const customReadFile = (file_path,res) => {
+    if(fs.existsSync(file_path)) {
+        fs.readFile(file_path, (error,data) => {
+            if(error){
+                console.log(error);
+                sendErrorResponse(res);
+                return;
+            }
+            res.write(data);
+            res.end();
+        });
+    } else {
+        sendErrorResponse(res);
+    }
+};
